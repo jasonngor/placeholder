693fd3d09c9ac1a1c1c651e43bf572091eca99c1
Sri Bhat <sribhat31@gmail.com>
2017-01-21 17:10:36 -0500

    Fixing directory structure

---
+++ AmazonRoulette/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='AmazonRoulette',
+    packages=['AmazonRoulette'],
+    include_package_data=True,
+    install_requires=[
+        'flask',
+    ],
+)
