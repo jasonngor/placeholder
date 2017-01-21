bc21b8ca7bf9d779eddc7f15e2098a8eba24bf84
Sri Bhat <sribhat31@gmail.com>
2017-01-21 15:38:06 -0500

    Fixing AmazonRoulette.py

--- AmazonRoulette/AmazonRoulette/AmazonRoulette.py
+++ AmazonRoulette/AmazonRoulette/AmazonRoulette.py
@@ -24,7 +24,6 @@ app.config.from_envvar('AMAZONROULETTE_SETTINGS', silent=True)
 def home():
     return render_template("index.html")
 
-<<<<<<< HEAD
 @app.route("/get-product", methods=[GET])
 def getproduct():
     found = False
@@ -44,45 +43,3 @@ def getproduct():
         except Exception:
             found = False
     return json.dump(product)
-=======
-# @app.teardown_appcontext
-# def close_db(error):
-#     """Closes the database at the end of the request."""
-#     if hasattr(g, 'sqlite_db'):
-#         g.sqlite_db.close()
-#
-# def init_db():
-#     db = get_db()
-#     with app.open_resource('schema.sql', mode='r') as f:
-#         db.cursor().executescript(f.read())
-#     db.commit()
-#
-# @app.cli.command('initdb')
-# def initdb_command():
-#     """Initializes the database."""
-#     init_db()
-#     print('Initialized the database.')
-#
-@app.route('/')
-def home():
-    """Takes user to index.html"""
-    return render_template("index.html")
-
-@app.route('/signup')
-def signup():
-    """Takes user to signup.html"""
-    return render_template("signup.html")
-
-@app.route("/signup", methods=["POST"])
-def adduser():
-    db = get_db()
-    db.execute("insert into user (username, password) values (?, ?)", [request.form["username"], request.form["password"]])
-    db.commit()
-    flash("New user added.")
-    return redirect(url_for("home"))
-
-@app.route('/login')
-def login():
-    """Takes user to login.html"""
-    return render_template("login.html")
->>>>>>> 6cdc385765987feb5f5939915c23e9e1ba170e8c
