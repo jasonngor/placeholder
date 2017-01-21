0121cb429199923e9a22b3d01d9719d30767c919
davidwang888 <davidwang888@gmail.com>
2017-01-21 12:19:33 -0500

    edited schema.sql

--- liketwitter/liketwitter/schema.sql
+++ liketwitter/liketwitter/schema.sql
@@ -4,3 +4,26 @@ create table entries (
   title text not null,
   'text' text not null
 );
+
+
+drop table if exists user;
+create table user (
+	user_id integer primary key autoincrement,
+	username text not null,
+	password text not null
+);
+
+drop table if exists follower;
+create table follwer(
+	follower_id integer,
+	following_id integer
+);
+
+drop table if exists message;
+create table message (
+	message_id integer primary key autoincrement,
+	author_id integer not null,
+	author_username text not null,
+	text text not null,
+	publish_date integer
+);
