84e5e738f37c6201e2e75dda14f18fd5c63bdc0f
RecursiveReincarnation <jswim26@gmail.com>
2017-01-21 19:23:57 -0500

    managed to link searchpage to product page

--- AmazonRoulette/app/AmazonRoulette.py
+++ AmazonRoulette/app/AmazonRoulette.py
@@ -6,23 +6,23 @@ from random import randint
 import json
 import os
 
-os.chdir("..") # change to file path
-os.chdir("..")
-
-f = open("pswd.txt", "r")
-st = f.read(200).strip("\n").split(" ")
-
-AMAZON_ACCESS_KEY = st[0]
-AMAZON_SECRET_KEY = st[1]
-AMAZON_ASSOC_TAG  = st[2]
-
-amazon = AmazonAPI(AMAZON_ACCESS_KEY, AMAZON_SECRET_KEY, AMAZON_ASSOC_TAG)
-product = amazon.lookup(ItemId='B00EOE0WKQ')
-print(product.title)
+# os.chdir("..") # change to file path
+# os.chdir("..")
+#
+# f = open("pswd.txt", "r")
+# st = f.read(200).strip("\n").split(" ")
+#
+# AMAZON_ACCESS_KEY = st[0]
+# AMAZON_SECRET_KEY = st[1]
+# AMAZON_ASSOC_TAG  = st[2]
+#
+# amazon = AmazonAPI(AMAZON_ACCESS_KEY, AMAZON_SECRET_KEY, AMAZON_ASSOC_TAG)
+# product = amazon.lookup(ItemId='B00EOE0WKQ')
+# print(product.title)
 
 app = Flask(__name__) # create the application instance
 app.config.from_object(__name__) # load config from this file, liketwitter.py
-amazon = AmazonAPI(AMAZON_ACCESS_KEY, AMAZON_SECRET_KEY, AMAZON_ASSOC_TAG)
+#amazon = AmazonAPI(AMAZON_ACCESS_KEY, AMAZON_SECRET_KEY, AMAZON_ASSOC_TAG)
 
 # Load default config and override config from an environment variable
 app.config.update(dict(
@@ -37,6 +37,7 @@ app.config.from_envvar('AMAZONROULETTE_SETTINGS', silent=True)
 def home():
     return render_template("index.html")
 
+@app.route("/product_page")
 def initAmazon():
     os.chdir("..") # change to file path
     os.chdir("..") # change to file path
@@ -52,27 +53,24 @@ def initAmazon():
     AMAZON_ASSOC_TAG  = st[2]
 
     amazon = AmazonAPI(AMAZON_ACCESS_KEY, AMAZON_SECRET_KEY, AMAZON_ASSOC_TAG)
-    return amazon
+    return redirect("www.amazon.com/dp/B00014D6QU")
 
-def getSpecificProduct():
-    pass
-
-@app.route("/get-product", methods=[GET])
-def getRandomProduct():
-    found = False
-    product = None
-    while not found:
-        asin = ""
-        letterstring = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-        for _ in range(10):
-            randomnum = randint(0,35)
-            if (randomnum <= 9):
-                asin += str(randomnum)
-            else:
-                asin += letterstring[randomnum - 10]
-        try:
-            product = amazon.lookup(asin)
-            found = True
-        except Exception:
-            found = False
-    return json.dump(product)
+# @app.route("/get-product", methods=[GET])
+# def getRandomProduct():
+#     found = False
+#     product = None
+#     while not found:
+#         asin = ""
+#         letterstring = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+#         for _ in range(10):
+#             randomnum = randint(0,35)
+#             if (randomnum <= 9):
+#                 asin += str(randomnum)
+#             else:
+#                 asin += letterstring[randomnum - 10]
+#         try:
+#             product = amazon.lookup(asin)
+#             found = True
+#         except Exception:
+#             found = False
+#     return json.dump(product)
--- AmazonRoulette/app/views.py
+++ AmazonRoulette/app/views.py
@@ -1,5 +1,5 @@
 from app import app
-from flask import render_template
+from flask import render_template, redirect
 
 @app.route('/')
 @app.route('/index')
@@ -9,3 +9,7 @@ def index():
 @app.route('/searchpage')
 def searchpage():
     return render_template("/searchpage.html")
+
+@app.route('/product_page')
+def product_page():
+    return redirect("http://www.amazon.com/dp/B00014D6QU")
